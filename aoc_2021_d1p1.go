9308cc45b7725d3ec8dd792f9a90cfc9248b82bc
Aaron Lael <alael@utah.gov>
2021-12-01 13:56:03 -0700

    Functional Day 1 parts 1 and 2

---
+++ aoc_2021_d1p1.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	lines, err := readinput(`C:\Users\alael\Documents\Go\AoC\2021\inp_d1.txt`)
+	if err != nil {
+		return
+	}
+	fmt.Println(ascend(lines))
+
+}
+
+func readinput(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+
+}
+
+func ascend(lines []string) int {
+	ascendcount := 0
+	for i, _ := range lines {
+		if i != 0 {
+			end, _ := strconv.Atoi(lines[i])
+			start, _ := strconv.Atoi(lines[i-1])
+			if end > start {
+				ascendcount += 1
+			}
+		}
+	}
+	return ascendcount
+}
---
+++ aoc_2021_d1p2.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+)
+
+func main() {
+	lines, err := readinput(`C:\Users\alael\Documents\Go\AoC\2021\inp_d1.txt`)
+	if err != nil {
+		return
+	}
+	fmt.Println(ascendtrio(lines))
+
+}
+
+func readinput(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
+
+}
+
+func ascendtrio(lines []string) int {
+	ascendcount := 0
+	for i, _ := range lines {
+		if i < len(lines)-3 {
+			c := thethree(lines[i], lines[i+1], lines[i+2])
+			n := thethree(lines[i+1], lines[i+2], lines[i+3])
+			if n > c {
+				ascendcount += 1
+			}
+		}
+	}
+	return ascendcount
+}
+
+func thethree(a string, b string, c string) int {
+	ia, _ := strconv.Atoi(a)
+	ib, _ := strconv.Atoi(b)
+	ic, _ := strconv.Atoi(c)
+	return ia + ib + ic
+}

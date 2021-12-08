eb22824011c87e215051e80c91a7463aeed26c4f
Aaron Lael <aaron.j.lael@gmail.com>
2021-12-08 11:27:02 -0700

    function day 5 p1

--- aoc_2021_d5.go
+++ aoc_2021_d5.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 	"time"
 )
 
@@ -13,36 +15,80 @@ func main() {
 	if err != nil {
 		return
 	}
-	coords := make(map[string]map[string]int)
-	for l, _ in range lines {
 
+	coords := make(map[string]map[string]int)
+	for _, l := range lines {
+		coords = addcoords(coords, l)
 	}
+	/// part 1
+	c := 0
+	for k, _ := range coords {
+		for k2, v := range coords[k] {
+			if v > 1 {
+				fmt.Println(k, k2)
+				c += 1
+			}
+		}
+	}
+	fmt.Println(c)
+	/// part 2
 	fmt.Println(time.Since(start))
 }
 
-
 func addcoords(coords map[string]map[string]int, coordset string) map[string]map[string]int {
 	sx, sy, ex, ey := coordform(coordset)
+	var offset int
 	if sx == ex {
-		diff := int(math.Abs(sy - ey))
 		if sy > ey {
-			
+			offset = -1
+		} else {
+			offset = 1
 		}
+		for sy != ey+offset {
+			coords = mapadd(coords, sx, sy)
+			sy += offset
+		}
+	}
+	if sy == ey {
+		if sx > ex {
+			offset = -1
+		} else {
+			offset = 1
+		}
+		for sx != ex+offset {
+			coords = mapadd(coords, sx, sy)
+			sx += offset
+		}
+	}
+	return coords
+}
 
+func mapadd(coords map[string]map[string]int, x int, y int) map[string]map[string]int {
+	strx := strconv.Itoa(x)
+	stry := strconv.Itoa(y)
+	if _, ok := coords[strx]; ok {
+		if _, ok := coords[strx][stry]; ok {
+			coords[strx][stry] += 1
+		} else {
+			coords[strx][stry] = 1
+		}
+	} else {
+		coords[strx] = make(map[string]int)
+		coords[strx][stry] = 1
 	}
-
-	if v1, ok := coords[x]; ok {
-		coords[x] := make(map[string]map[string]int)
-	}
-	if v2, ok := coords[x][y]; ok {
-		coords[x][y] := make(map[string]int)
-	}
+	return coords
 }
 
 func coordform(coordset string) (int, int, int, int) {
-	s,e := strings.Split(coord, " -> ")
-	sx, sy := strings.Split(s, ",")
-	ex, ey := strings.Split(e, ",")
+	t1 := strings.Split(coordset, " -> ")
+	s := t1[0]
+	e := t1[1]
+	ts := strings.Split(s, ",")
+	sx := ts[0]
+	sy := ts[1]
+	te := strings.Split(e, ",")
+	ex := te[0]
+	ey := te[1]
 	isx, _ := strconv.Atoi(sx)
 	isy, _ := strconv.Atoi(sy)
 	iex, _ := strconv.Atoi(ex)
